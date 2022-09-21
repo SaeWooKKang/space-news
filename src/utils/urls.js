dc8be59248bac3631a328d55e2609b2b3a000350
SaeWooKKang <87258182+SaeWooKKang@users.noreply.github.com>
2022-09-21 15:55:51 +0900

    feat: 이미지 슬라이드 구현

---
+++ src/utils/urls.js
@@ -0,0 +1,7 @@
+export const slider_imgs = [
+  "https://live.staticflickr.com/65535/49635401403_96f9c322dc_o.jpg",
+  "https://live.staticflickr.com/65535/49636202657_e81210a3ca_o.jpg",
+  "https://live.staticflickr.com/65535/49636202572_8831c5a917_o.jpg",
+  "https://live.staticflickr.com/65535/49635401423_e0bef3e82f_o.jpg",
+  "https://live.staticflickr.com/65535/49635985086_660be7062f_o.jpg"
+];
